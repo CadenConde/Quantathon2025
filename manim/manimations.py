9dbe361d82ad7dc6f77ea669cb143fd644eeffb2
ryjl33 <rlee0292@gmail.com>
2025-03-01 15:14:47 -0500

    manim test

--- manim/manimations.py
+++ manim/manimations.py
@@ -1,12 +1,44 @@
-from manim import *;
+from manim import *
 
-#To Run (in terminal):
-# "manim -pql manimations.py demo"
-class demo(Scene):
+class ThreePlanesScene(ThreeDScene):
     def construct(self):
-        t = Text("Hello!").shift(UP)
-        t2 = Tex("Hi!").shift(DOWN)
-        self.play(Write(t), Write(t2))
+        # Set up the 3D axes
+        axes = ThreeDAxes()
+
+        # Define three planes using ParametricSurface
+        plane1 = ParametricSurface(
+            lambda u, v: np.array([u, v, 1]),
+            u_range=[-3, 3],
+            v_range=[-3, 3],
+            resolution=(20, 20),
+            checkerboard_colors=[BLUE, BLUE_E],
+            fill_opacity=0.5
+        )
+
+        plane2 = ParametricSurface(
+            lambda u, v: np.array([u, v, -u - v]),
+            u_range=[-3, 3],
+            v_range=[-3, 3],
+            resolution=(20, 20),
+            checkerboard_colors=[RED, RED_E],
+            fill_opacity=0.5
+        )
+
+        plane3 = ParametricSurface(
+            lambda u, v: np.array([u, v, 0.5 * u - v]),
+            u_range=[-3, 3],
+            v_range=[-3, 3],
+            resolution=(20, 20),
+            checkerboard_colors=[GREEN, GREEN_E],
+            fill_opacity=0.5
+        )
+
+        # Set the camera orientation
+        self.set_camera_orientation(phi=75 * DEGREES, theta=45 * DEGREES)
+
+        # Add objects to scene
+        self.add(axes, plane1, plane2, plane3)
+
+        # Hold the scene
         self.wait(3)
-        self.play(FadeOut(t), FadeOut(t2))
-        self.wait(1)
+
